2cd88511e2c1c98b4497dc56b1d0386abb39bd6f
ajfm88 <ajfoucaultmo@mail.usf.edu>
2022-02-05 21:31:47 -0800

    60. Alert Component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
+import Alert from './components/layout/Alert'
 import Home from './pages/Home'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
-import {GithubProvider} from './context/github/GithubContext'
+import { GithubProvider } from './context/github/GithubContext'
+import { AlertProvider } from './context/alert/AlertContext'
 
 function App() {
   return (
     <GithubProvider>
-      <Router>
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
+      <AlertProvider>
+        <Router>
+          <div className='flex flex-col justify-between h-screen'>
+            <Navbar />
 
-          <main className='container mx-auto px-3 pb-12'>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/about' element={<About />} />
-              <Route path='/notfound' element={<NotFound />} />
-              <Route path='/*' element={<NotFound />} />
-            </Routes>
-          </main>
+            <main className='container mx-auto px-3 pb-12'>
+              <Alert />
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route path='/about' component={About} />
+                <Route path='/notfound' component={NotFound} />
+                <Route component={NotFound} />
+              </Switch>
+            </main>
 
-          <Footer />
-        </div>
-      </Router>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   )
 }
 
-export default App;
+export default App
