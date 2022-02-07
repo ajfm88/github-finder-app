f20b13732b52e90719b11955bae3c7c1ba50751d
ajfm88 <ajfoucaultmo@mail.usf.edu>
2022-02-06 22:49:30 -0800

    68. Finish GitHub Finder App

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -20,5 +20,5 @@ function Footer() {
     </footer>
   )
 }
-  
+
 export default Footer
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui')],
-  daisyui: {
-    themes: ['dark'],
+  variants: {
+    extend: {},
   },
+  plugins: [require('daisyui')],
 }
